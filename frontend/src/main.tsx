e4c2a6769c2327d1371f909464a920314bc41d5b
Karin <nockykarin@gmail.com>
2025-05-22 17:22:29 +0900

    fix:ログインできるようにした

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_gOVy1Nd3f",
   client_id: "6853gvj2etkjscvolje3mnrifc",
-  redirect_uri: "http://localhost/",
+  redirect_uri: "http://localhost:5173",
   response_type: "code",
   scope: "email openid phone",
 };
