38924beaccbec7231793567bb8b252ba286585bd
Gata425 <107904801+Gata425@users.noreply.github.com>
2025-05-22 12:38:15 +0900

    cdの作成をしたよ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -47,7 +47,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React aiueo</h1>
       <h2>{message}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
