fb12ce80bb64081d2ff5f5dc06b501f8b1813fda
Karin <nockykarin@gmail.com>
2025-05-22 16:50:03 +0900

    cognitoの設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,38 @@ import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { useAuth } from "react-oidc-context";
 
 function App() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "6853gvj2etkjscvolje3mnrifc";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://ap-northeast-1govy1nd3f.auth.ap-northeast-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
   const [count, setCount] = useState(0);
   const [message, setMessage] = useState<string>('');
   async function getData(): Promise<{ message: string } | undefined> {
@@ -39,6 +69,10 @@ function App() {
 
   return (
     <>
+      <div>
+        <button onClick={() => auth.signinRedirect()}>Sign in</button>
+        <button onClick={() => signOutRedirect()}>Sign out</button>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_gOVy1Nd3f",
+  client_id: "6853gvj2etkjscvolje3mnrifc",
+  redirect_uri: "http://localhost/",
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
