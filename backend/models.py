801cc221e0ce408a51c1d0957a62889470a2801b
bondroid1322 <bondroid3.22@icloud.com>
2025-06-10 20:07:45 +0900

    add: user_idカラムにテーブルのスキーマ定義同様に外部キー制約を追加

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Text, TIMESTAMP, func
+from sqlalchemy import Column, String, Text, TIMESTAMP, func, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
@@ -10,7 +10,7 @@ class Blog(Base):
     __table_args__ = {"schema": "public"}  # スキーマ指定（必要なら）
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('public.users.id'), nullable=False)
     title = Column(String(255), nullable=False)
     content = Column(Text, nullable=False)
     created_at = Column(
