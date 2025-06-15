00499c7c42bc9a9c3a61d72143531ff40302f91b
bondroid1322 <bondroid3.22@icloud.com>
2025-06-15 17:45:29 +0900

    add: ignoreを設定

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,8 +6,16 @@ import pluginPrettier from 'eslint-plugin-prettier';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  js.configs.recommended, //  Flat Config ではこう書く
-
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      'coverage/**',
+      '.git/**',
+    ],
+  },
+  js.configs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 
